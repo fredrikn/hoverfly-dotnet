9d72c8fe65e5388154495063a9f534b1bb7afe1b
Fredrik Normén <fnormen@hotmail.com>
2017-02-17 15:46:10 +0100

    Added hoverfly.dsl project.

---
+++ src/Hoverfly.Dsl/HoverflyDsl.cs
@@ -0,0 +1,14 @@
+﻿namespace Hoverfly.Dsl
+{
+    public class HoverflyDsl
+    {
+        private HoverflyDsl()
+        {
+        }
+
+        //public static StubServiceBuilder service(string baseUrl)
+        //{
+        //    return new StubServiceBuilder(baseUrl);
+        //}
+    }
+}
